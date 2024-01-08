b4f850ad9185a316130385e78362cad43e3245e0
Naomi Shvedov <54858317+naomish1230@users.noreply.github.com>
2024-01-08 13:15:06 -0500

    made final collective migration figure version

--- CollectiveMigrationFig/CollectiveMigrationFigureCorrelations.m
+++ CollectiveMigrationFig/CollectiveMigrationFigureCorrelations.m
@@ -1,29 +1,21 @@
-% Collective Migration Figure: Correlations
+% Collective Migration Figure: Correlations% Collective Migration Figure: Correlations
 % using 5 high temporal resolution birds. 
 
-%==========  USER INPUT 
-%spec_birds = [5,11,3,4,61]; %all birds!
-spec_birds = [4,11,3,61]; %something weird happens when add in lime5L because of some NaNs
-%============
-
-clearvars -except spec_birds
-addpath('/Users/naomishvedov/Documents/GitHub/In-vivo-Migratory-Dynamics/CollectiveMigrationFig/')
-load('birds_headCorrs_table.mat')
-load('birds_speedCorr_table.mat')
-
-birds_headCorrs_table = birds_headCorrs_table(ismember(birds_headCorrs_table.ID,spec_birds),:);
-birds_speedCorr_table = birds_speedCorr_table(ismember(birds_speedCorr_table.ID, spec_birds),:);
+clearvars
+addpath('/Users/naomishvedov/Documents/GitHub/Transgenic-songbird-neuron-migration/CollectiveMigrationFig/')
+load('birds_headCorrs_table.mat') %generated angle difference between pairs of cells, n = 5 birds, done for each bird and session separately then concatenated. 
+load('birds_speedCorr_table.mat')%generated Spearman correlations of 5 birds' cells' interpolated velocities. pairs produced within bird and within session. 
 
 %% Heading Correlations 
 
-%remove correlations that are with self
+%remove correlations that are with self (cell pair < 5 um away) 
 birds_headCorrs_table(birds_headCorrs_table.DistDiffs < 5,:) = [];
 
 %are correlations in the population above chance?
+%shuffled distribution vs. data distribution histograms
 figure()
 h = histogram(birds_headCorrs_table.AngleDiff,36,'FaceColor',"black",'EdgeColor','none','FaceAlpha',0.4); %DATA
 hold on
-%histogram(birds_headCorrs_table.ShuffAngleDiff,'FaceColor',"yellow",'FaceAlpha',0.55)
 [counts,centers] = hist(birds_headCorrs_table.ShuffAngleDiff,h.NumBins); %shuffled data
 plot(centers,counts,'LineWidth',3,'Color','blue') %SHUFFLED DATA LINE plotted
 set(gca,'FontSize',16), set(gcf,'color','w');
@@ -33,7 +25,7 @@ title(['Angle Differences, n = ' num2str(height(birds_headCorrs_table)) ' pairs,
 ylabel('Pair Count')
 xlabel('Degrees')
 
-% is there a relationship between correlation and distance?
+% is there a relationship between correlation and distance? linear regression 
 angmdl = fitlm(reshape(birds_headCorrs_table.DistDiffs,1,[]), reshape(birds_headCorrs_table.AngleDiff,1,[]))
 angmdl_shuff = fitlm(reshape(birds_headCorrs_table.ShuffDistDiffs,1,[]), reshape(birds_headCorrs_table.ShuffAngleDiff,1,[]))
 
